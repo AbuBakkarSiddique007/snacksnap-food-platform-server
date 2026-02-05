6c665b55981496a2b5ef52f84e277388f7bd4d91
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2026-02-06 03:27:33 +0600

    chore: initialize project structure

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import app from "./app";
+import { prisma } from './lib/prisma';
+
+const port = process.env.PORT || 5000
+
+async function server() {
+    try {
+        await prisma.$connect()
+        console.log("Database Connected Successfully");
+
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+
+    } catch (error) {
+        console.error('Failed to start the server:', error);
+
+        await prisma.$disconnect()
+        process.exit(1);
+    }
+}
+
+server();
